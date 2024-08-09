9e95564f320ae71eb3d2caef9ec14662775c6921
Anyidollar <ifeanyionyekwere247@gmail.com>
2024-08-10 00:19:01 +0100

    factor responsiveness on the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,30 @@
 import More from "../assets/DiscoverMore.svg";
-// import Navbar from "./NavBar";
-// import Heading from "./Heading";
-import { IoIosArrowDroprightCircle } from "react-icons/io";
 import Icon from "../assets/Icons.svg";
 
 const Header = () => {
   return (
     <>
-      <header className="bg-gradient-to-r from-indigo-800 from-10% via-sky-700 via-30% to-emerald-900 to-90% mt-[0px] py-[20px] h-[920px]">
-        <div className="relative w-30 h-[300px] mt-[-50px] mb-[250px] ">
-          <div className="w-1/5 absolute flex justify-between left-60 -top-6 px-6 py-4 bg-gray-800 text-center rounded-lg text-white">
-            <p>Discover More</p>
-            <a>
-              <IoIosArrowDroprightCircle className="text-2xl  bg-black rounded-2xl" />
-            </a>
-          </div>
-          <img
-            className="Image1 -z-10"
-            src={More}
-            alt="Discover More"
-            loading="lazy"
-          />
+      <header className="relative bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% mt-[0px] ">
+        <div className="">
+          <p className=" md:w-1/5  max-sm:ml-6 absolute md:left-60 -top-6 p-4  bg-gray-500 text-center rounded-lg">
+            Discover More
+          </p>
+          <img className="" src={More} alt="Discover More" />
         </div>
-        <div className="h-auto bg-[#FFFFFF] shadow-lg w-[90%] ml-[5%] rounded-t-lg mt-[-100px] z-1000">
+
+        <div className="h-auto absolute -bottom-20 bg-[#FFFFFF] md:shadow-lg md:w-[90%] md:ml-[5%] rounded-t-lg ">
           <div className="rounded-t-lg py-4 px-2  ">
-            <p className="py-4 px-2">
+            <p className="md:py-4 px-2">
               Send and Receive Money from all over with ease.
             </p>
           </div>
-          <div className="px-32 ">
-            <img className="" src={Icon} alt="Icon" />
+          <div className="md:px-32">
+            <img src={Icon} alt="Icon" />
           </div>
         </div>
       </header>
     </>
   );
 };
+
 export default Header;
--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -2,13 +2,13 @@ import Laptop from "../assets/LaptopFrame.svg";
 
 const Heading = () => {
   return (
-    <div className="flex justify-center w-[100%] mt-20">
-      <div className="flex flex-col justify-left item-center pl-[150px] pt-[20px] md:w-[60%] sm:w-[100%]">
+    <div className="flex bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 flex-wrap mb-10 justify-center w-[100%] mt-20">
+      <div className=" flex flex-col justify-left item-center max-sm:p-10 md:pl-[150px] pt-[20px] md:w-[60%]">
         <p className="text-sm text-yellow-300">
           Simplified cross-border payment
         </p>
-        <p className="text-8xl text-white mt-1 mb-3">
-          We are the payments people
+        <p className="md:text-8xl sm:text-4xl text-white mt-1 mb-3">
+          We are the <br /> payments people
         </p>
         <p className="text-white text-sm">
           Transalliance Group.’s cross-border payments solutions connect <br />
@@ -16,17 +16,11 @@ const Heading = () => {
           to thrive. Together, we make money move.
         </p>
       </div>
-      <div className="flex flex-col relative md:w-[40%] sm:w-[100%] pt-[20px]">
-        <div>
-          <img
-            className="mt-[-135px] ml-10"
-            src={Laptop}
-            alt="Header"
-            loading="lazy"
-          />
-        </div>
-        <div className="absolute bg-[#FFFFFFB2] top-10 p-2 rounded-lg left-0 h-[70px] mt-3">
-          <p className="flex item-center justify-center pt-3.5">
+      <div className="md:flex md:flex-col relative md:w-[40%] md:pt-[20px]">
+        <img className="max-sm:ml-5 " src={Laptop} alt="Header" />
+
+        <div className="absolute bg-[#FFFFFFB2] max-sm:top-28 md:top-10 p-2 rounded-lg md:left-0 md:h-[70px] max-sm:h-[50px] mt-3">
+          <p className="flex item-center justify-center max-sm:pt-2 md:pt-3.5">
             24/7, 365 transaction tracking
           </p>
         </div>
